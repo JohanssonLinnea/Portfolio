ea6b05146a14061e473f4d07a977a4cf859e7a57
Linnea Johansson <Linneajoh@gmail.com>
2024-05-29 21:17:40 +0200

    första push

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Headline from './Components/Headline'; 
+import Tech from './Components/Tech';
+import Projects from './Components/Projects';
+import Skills from './Components/Skills';
+import Talk from './Components/Talk';
+import Footer from './Components/Footer';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Headline />
+      <Tech />
+      <Projects />
+      <Skills />
+      <Talk />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
